4e2f71b79caea6392a5a66487b6338155d636c3a
luudinhchinh <chinhluu99@gmail.com>
2022-06-19 21:58:49 +0700

    demo

--- reactjs/jsx/demo/src/index.js
+++ reactjs/jsx/demo/src/index.js
@@ -1,9 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const name = "my name"
-const renderdom = React.createElement("h1",{
-  style:{color:'red'}
-},name)
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(renderdom)
+// const fruit =  ["Apple", "Banana", "Orange", "Apricot", "Black rowan", "Cranberry"];
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(
+//   <div>
+//     <h1>list of fruit</h1>
+//     <ul>
+//       {fruit.map((item)=>(
+//         <li>{item}</li>
+//       ))}
+//     </ul>
+//   </div>
+// )
+
+// thực hành hiển thị thời gian hiện tại
+//  const root = ReactDOM.createRoot(document.getElementById('root'))
+//  const reder = ()=>{ root.render(
+//   <div>
+//     <h1>hello, word</h1>
+//     <b>it is {new Date().toLocaleTimeString()}</b>
+//   </div>
+//  )}
+//  setInterval(reder,1000)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <div>Browser's details: {navigator.userAgent}</div>
+)
